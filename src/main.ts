8f92dad84032a794cdaa8d1d483e4caff46f8e77
Titus <dev@tituscoleman.com>
2023-12-17 23:55:52 -0500

    add token metadata

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import { keypairIdentity } from "@metaplex-foundation/umi";
 
 const umi = createUmi('https://api.devnet.solana.com');
 
-const keypair = umi.edds.createKeypairFromSecretKey(userKeypair.secretKey);
+const keypair = umi.eddsa.createKeypairFromSecretKey(userKeypair.secretKey);
 
 umi.use(keypairIdentity(keypair))
 	.use(mplTokenMetadata())
+
+
+
+const metadata = {
+    name: "Vin Moon",
+    symbol: "VMO",
+    uri: "https://raw.githubusercontent.com/Titus-Coleman/metaplex-umi-token/main/src/test-asset.json",
+};
+
