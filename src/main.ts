164a7e955ebda52029680cb254a8e7038fe9a212
Titus <dev@tituscoleman.com>
2023-12-18 16:06:52 -0500

    working prototype

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { userKeypair } from "./helpers";
-import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
-import { keypairIdentity } from "@metaplex-foundation/umi";
+import { mplTokenMetadata, createV1, TokenStandard, mintV1, createAndMint  } from "@metaplex-foundation/mpl-token-metadata";
+import { keypairIdentity, generateSigner, percentAmount, signerIdentity, signerPayer } from "@metaplex-foundation/umi";
 
 const umi = createUmi('https://api.devnet.solana.com');
 
 const keypair = umi.eddsa.createKeypairFromSecretKey(userKeypair.secretKey);
 
 umi.use(keypairIdentity(keypair))
-	.use(mplTokenMetadata())
-
+.use(mplTokenMetadata())
+	
 
 
 const metadata = {
@@ -18,3 +18,50 @@ const metadata = {
     uri: "https://raw.githubusercontent.com/Titus-Coleman/metaplex-umi-token/main/src/test-asset.json",
 };
 
+
+// https://developers.metaplex.com/token-metadata/mint
+
+const mint = generateSigner(umi);
+
+async function createMyToken() {
+    await createAndMint(umi, {
+        mint,
+        authority: umi.identity,
+        name: metadata.name,
+        symbol: metadata.symbol,
+        uri: metadata.uri,
+        sellerFeeBasisPoints: percentAmount(0),
+        decimals: 9,
+        amount: 10_000,
+        tokenOwner: umi.identity.publicKey,
+        tokenStandard: TokenStandard.Fungible,
+    }).sendAndConfirm(umi).then(() => {
+        console.log("Success VMO (", mint.publicKey, ") minted");
+    })
+// async function createMetadataDetails() {
+//     await createV1(umi, {
+//         mint,
+//         authority: umi.identity,
+//         name: metadata.name,
+//         symbol: metadata.symbol,
+//         uri: metadata.uri,
+//         sellerFeeBasisPoints: percentAmount(0),
+//         decimals: 9,
+//         tokenStandard: TokenStandard.Fungible,
+//     }).sendAndConfirm(umi)
+}
+
+// async function mintToken() {
+//     await mintV1(umi, {
+//         mint: mint.publicKey,
+//         authority: umi.identity,
+//         amount: 10_000,
+//         tokenOwner: umi.identity.publicKey,
+//         tokenStandard: TokenStandard.Fungible,
+//     }).sendAndConfirm(umi)
+// }
+
+// createMetadataDetails()
+// mintToken()
+
+createMyToken()
